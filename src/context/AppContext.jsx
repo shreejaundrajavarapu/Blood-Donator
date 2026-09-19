0962506ff62fc42f85001a70eaa4f6cdb4baccb2
bhuvana <bhuvaneswarivandana@gmail.com>
2026-09-19 10:41:02 +0530

    Connect frontend to Render backend

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const AppContext = createContext();
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://blood-connecter.onrender.com/api';
 
 const INITIAL_USERS = [];
 const INITIAL_REQUESTS = [];
